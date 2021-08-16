08fd95bbf87426fa14d5edd9aebe0bf6f586caef
Nico <87850895+NicoSalina@users.noreply.github.com>
2021-08-16 14:21:41 -0300

    proyecto v.1.2

--- routes/productos.js
+++ routes/productos.js
@@ -6,14 +6,14 @@ const {getAll, getSingle} = require('../models/productos');
 const all = async (req, res) => {
   user = req.session.name;
   const productos = await getAll();
-  res.render('productos', {titulo: ' a Entrenamiento STP', productos, user});
+  res.render('productos', {titulo: ' Clases', productos, user});
 }
 
 const single = async (req, res) => {
   user = req.session.name;
   const {id} = req.params;
   const [producto] = await getSingle(id);
-  res.render ('singleProduct', {titulo: ' a Entrenamiento STP', producto, user});
+  res.render ('singleProduct', {titulo: ' Clases', producto, user});
 }
 
 router.get('/singleProduct/:id', single);
